1ac0c295da66e42d337c2fe77dc199b1464e4c30
dima home <dimanok88@mail.ru>
2016-12-06 23:12:49 +0300

    Patterns Factory  end

--- factory.js
+++ factory.js
@@ -1,34 +1,48 @@
+var FullTime = function () {
+    this.hourly = "$12";
+};
+
+var PartTime = function () {
+    this.hourly = "$11";
+};
+
+var Temporary = function () {
+    this.hourly = "$10";
+};
+
+var Contractor = function () {
+    this.hourly = "$15";
+};
+
 var Factory = {
-    log: function () {
-        var log = "";
-        return {
-            add: function(message){
-                log += message + "\n";
-            },
-            show: function () {
-                console.log(log);
-                log = "";
-            }
-        }
-    },
     types:{
         fullTime: function () {
-
+            return new FullTime();
         },
         partTime : function () {
-
+            return new PartTime();
         },
         temporary:function () {
-
+            return new Temporary();
         },
         contractor: function(){
-
+            return new Contractor();
         }
     },
-    say: function () {
-        this.log.add(this.)
-    },
-    createStaff : function (type) {
+    createStaff: function (type) {
         return new this.types[type]();
     }
 };
+
+var f = Factory;
+var staff = [];
+
+staff.push(f.createStaff("fullTime"));
+staff.push(f.createStaff("partTime"));
+staff.push(f.createStaff("temporary"));
+staff.push(f.createStaff("contractor"));
+
+
+for (var i = 0, len = staff.length; i < len; i++) {
+    console.log(staff[i]);
+}
